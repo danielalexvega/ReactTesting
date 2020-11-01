ae3a1842ed09fccf0b5a51d372ea6679128f24a9
Daniel Vega <danielalexvega@gmail.com>
2020-11-01 17:11:29 -0600

    initial commit into Testing repo

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div>
+        Hello Testing
+      </div>
+    )
+  }
+};
+
+export default App;
