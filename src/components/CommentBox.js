56bff202107435aefaa97b43e0811ecdaf645ff8
Daniel Vega <danielalexvega@gmail.com>
2020-11-02 11:34:30 -0600

    complete tests in CommentBox.test.js

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -9,18 +9,21 @@ class CommentBox extends React.Component {
 
   handleSubmit = (event) => {
     event.preventDefault();
-    
+
     // TODO 
     // Call an action creator and save the comment 
 
-    this.setState({ comment: ''})
+    this.setState({ comment: '' })
   }
 
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
         <h4>Add a Comment</h4>
-        <textarea value={this.state.comment} onChange={this.handleChange} />
+        <textarea
+          value={this.state.comment}
+          onChange={this.handleChange}
+        />
         <div>
           <button>Submit Comment</button>
         </div>
